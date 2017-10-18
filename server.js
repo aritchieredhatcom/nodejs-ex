48e1995b13f5e53ba80f61aeea05d88e22a134db
aritchieredhatcom <aritchie@redhat.com>
2017-10-18 13:56:06 +1000

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/setnvpair', function (req, res) {
+app.get('/getnvpair', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
     
